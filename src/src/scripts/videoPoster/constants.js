bc1714c205eefde7fb4928fbc5a49833d29ee3eb
Gareth Wright <garpunkal@gmail.com>
2026-03-27 10:02:07 +0000

    refactor: enhance YouTube iframe allow attributes and clean up unused code in createIframe function

--- src/src/scripts/videoPoster/constants.js
+++ src/src/scripts/videoPoster/constants.js
@@ -1,4 +1,4 @@
 export const ALLOW = {
-  youtube: "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; web-share",
+  youtube: "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; web-share; picture-in-picture",
   vimeo: "autoplay; fullscreen; "
 };
